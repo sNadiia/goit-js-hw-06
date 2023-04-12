dd619e2b2384a2adb1bb9ea1e1c5c3edbc0228a2
Nadiia Samborska <dilova_3@ukr.net>
2023-04-12 13:14:03 +0300

    Update task-02.js
    done 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.getElementById("ingredients");
+const fragment = document.createDocumentFragment();
+
+for (const ingredient of ingredients) {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  fragment.appendChild(liEl);
+}
+ulEl.appendChild(fragment);
