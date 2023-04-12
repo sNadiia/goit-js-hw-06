e6a5e3fe428a0e7c5230e96cfe0d37b31bf32f2a
Nadiia Samborska <dilova_3@ukr.net>
2023-04-12 17:41:35 +0300

    task -3
    done

--- js/task-03.js
+++ js/task-03.js
@@ -14,23 +14,13 @@ const images = [
 ];
 
 const galleryEl = document.querySelector(".gallery");
-// const fragment = document.createDocumentFragment();
-
-// images.forEach(({ url, alt }) => {
-//   const itemGalleryEl = document.createElement("li");
-//   const imageEl = document.createElement("img");
-//   itemGalleryEl.appendChild(imageEl);
-//   imageEl.src = url;
-//   imageEl.alt = alt;
-//   imageEl.width = 400;
-//   imageEl.height = 225;
-//   fragment.appendChild(itemGalleryEl);
-// });
-// galleryEl.appendChild(fragment)
 
 const markup = images
-  .map(({url, alt}) => `<li class="list-item"><img src = ${url} alt = ${alt}></li>`)
+  .map(
+    ({ url, alt }) =>
+      `<li><img src = ${url} alt = ${alt}></li>`
+  )
   .join("");
 
 galleryEl.insertAdjacentHTML("beforeend", markup);
-// list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
+
