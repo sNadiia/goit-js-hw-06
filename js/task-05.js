35dfa2a9b82bb415eaf3adec1f0d7c7041e400a5
Nadiia Samborska <dilova_3@ukr.net>
2023-04-14 22:51:17 +0300

    5 and 8

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,8 @@
 const inputEl = document.getElementById("name-input");
+console.log(inputEl);
 const titleOutputEl = document.getElementById("name-output");
 function renameOutput(event) {
-  if (event.currentTarget.value != false) {
+  if (event.currentTarget.value != false ) {
     titleOutputEl.textContent = event.currentTarget.value;
   } else {
     titleOutputEl.textContent = "Anonymous";
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value && password.value) {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+    return formData;
+  } else {
+    alert("всі поля повинні бути заповнені");
+  }
+});
