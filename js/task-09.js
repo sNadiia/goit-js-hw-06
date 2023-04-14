ded16198b69350ba443d1aaf6173e3a8e712d686
Nadiia Samborska <dilova_3@ukr.net>
2023-04-14 23:27:34 +0300

    Update task-09.js
    done 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const buttonChangeColor = document.querySelector(".change-color");
+const currentColorValue = document.querySelector(".color");
+const body = document.querySelector("body");
+
+buttonChangeColor.addEventListener("click", onClickButton);
+function onClickButton(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  currentColorValue.textContent = body.style.backgroundColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+console.log(getRandomHexColor());
