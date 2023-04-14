1a32b5dc6ec866e00f65bd65c74d5b001b3e4439
Nadiia Samborska <dilova_3@ukr.net>
2023-04-14 15:49:22 +0300

    Update task-07.js
    Done 7

--- js/task-07.js
+++ js/task-07.js
@@ -3,6 +3,5 @@ const textToChangeEl = document.getElementById("text");
 
 inputRangeEl.addEventListener("input", onTextSizeChange);
 function onTextSizeChange(event) {
-  console.dir(event.currentTarget.value);
   textToChangeEl.style.fontSize = `${event.currentTarget.value}px`;
 }
