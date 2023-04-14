91b806e629b69e3a36e358d538abc2d862b61a0e
Nadiia Samborska <dilova_3@ukr.net>
2023-04-14 13:14:05 +0300

    Update task-05.js
    add if else

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,10 @@
 const inputEl = document.getElementById("name-input");
 const titleOutputEl = document.getElementById("name-output");
 function renameOutput(event) {
-  titleOutputEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value == 0) {
+    titleOutputEl.textContent = Anonymous;
+  } else {
+    titleOutputEl.textContent = event.currentTarget.value;
+  }
 }
 inputEl.addEventListener("input", renameOutput);
